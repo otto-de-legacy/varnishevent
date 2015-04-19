1779d9023745fd767200438835988cd7e03fb275
Geoff Simmons <geoff@uplex.de>
2015-04-19 14:48:32 +0200

    add more tests for the -g option

--- src/test/vslarg.sh
+++ src/test/vslarg.sh
@@ -4,8 +4,7 @@ echo
 echo "TEST: $0"
 echo "... testing standard VSL args"
 
-echo "... -g grouping"
-
+echo "... -g vxid"
 # For grouping, use the larger test log that has some backend transactions,
 # since the two groupings have the effect of exchanging the order of some
 # output lines (no difference with the smaller log).
@@ -17,10 +16,29 @@ if [ "$CKSUM" != '3589078266 20793576' ]; then
     exit 1
 fi
 
+echo "... -g request"
 CKSUM=$( ../varnishevent -g request -r sw-doc.log -f varnishevent.conf | cksum)
 
 if [ "$CKSUM" != '2107709581 20793576' ]; then
     echo "ERROR: -g request unexpected cksum: $CKSUM"
+    exit 1
+fi
+
+echo "... -g raw"
+# Timestamps for raw grouping are always the time at which the tx was read,
+# even for binary file reads. So we check against the last three columns.
+CKSUM=$( ../varnishevent -g raw -r varnish-doc.log -f raw.conf | awk '{print $(NF-2), $(NF-1), $NF}' | cksum)
+
+if [ "$CKSUM" != '2652535054 3311' ]; then
+    echo "ERROR: -g raw unexpected cksum: $CKSUM"
+    exit 1
+fi
+
+echo '... -g session'
+../varnishevent -g session
+
+if [ "$?" != "1" ]; then
+    echo "ERROR: -g session did not exit with failure as expected"
     exit 1
 fi
 
